6e30d9066c845664464d441b232a4d029c050a10
Tommy Sparber <tommy@sparber.net>
2016-01-13 12:01:02 +1100

    Add mini test to curl-check-url.rb

--- Formula/curl-check-url.rb
+++ Formula/curl-check-url.rb
@@ -1,10 +1,14 @@
 class CurlCheckUrl < Formula
-  desc "Script to check the HTTP status code of a given URL using GET"
+  desc "Check the HTTP status code of a given URL using GET"
   homepage "https://github.com/tsparber/homebrew-tiny-scripts"
   url "https://github.com/tsparber/homebrew-tiny-scripts.git"
   version "0.1.0"
 
   def install
     bin.install "bin/curl-check-url"
+  end
+
+  test do
+    system bin/"curl-check-url", "https://github.com"
   end
 end
