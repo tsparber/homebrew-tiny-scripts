a90fff07cf22d0a06eb0a0d034078f5f1be75e63
Tommy Sparber <tommy@sparber.net>
2016-01-13 13:47:12 +1100

    curl-check-url: Add option to not follow location headers

--- Formula/curl-check-url.rb
+++ Formula/curl-check-url.rb
@@ -2,7 +2,7 @@ class CurlCheckUrl < Formula
   desc "Check the HTTP status code of a given URL using GET"
   homepage "https://github.com/tsparber/homebrew-tiny-scripts"
   url "https://github.com/tsparber/homebrew-tiny-scripts.git"
-  version "0.1.1"
+  version "0.1.2"
 
   def install
     bin.install "bin/curl-check-url"
