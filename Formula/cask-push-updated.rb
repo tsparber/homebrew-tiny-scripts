400eb08065dd6229ff04c1c9b40f3aacd89e08f8
Tommy Sparber <tommy@sparber.net>
2016-01-19 09:50:39 +1100

    cask-push-updated: Fix sending hub pull request

--- Formula/cask-push-updated.rb
+++ Formula/cask-push-updated.rb
@@ -2,7 +2,7 @@ class CaskPushUpdated < Formula
   desc "This script pushes the given cask to GitHub"
   homepage "https://github.com/tsparber/homebrew-tiny-scripts"
   url "https://github.com/tsparber/homebrew-tiny-scripts.git"
-  version "0.1.2"
+  version "0.1.3"
 
   def install
     bin.install "bin/cask-push-updated"
