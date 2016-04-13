5ae4ec074a35495a65b8d2075c37926a6f97940b
Tommy Sparber <tommy@sparber.net>
2016-04-13 08:51:05 +0200

    appcast-from-url: Follow redirects when loading the appcast

--- Formula/appcast-from-url.rb
+++ Formula/appcast-from-url.rb
@@ -2,7 +2,7 @@ class AppcastFromUrl < Formula
   desc "This script generates a appcast stanza for the given url"
   homepage "https://github.com/tsparber/homebrew-tiny-scripts"
   url "https://github.com/tsparber/homebrew-tiny-scripts.git"
-  version "0.1.0"
+  version "0.1.1"
 
   def install
     bin.install "bin/appcast-from-url"
